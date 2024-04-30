1a2bc7dd16a6762142e25344bd8984fd85187f0f
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2024-04-30 01:56:52 -0600

    Actualizacion 29-04-2024

---
+++ ProyectoFinal_V6/lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:proyectofinal/navegador.dart';
+import 'package:proyectofinal/vistas/calendario.dart';
+import 'package:proyectofinal/vistas/iniciarSesion.dart';
+import 'package:proyectofinal/vistas/pruebas.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App Juego Persona',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+        useMaterial3: true,
+      ),
+      locale: const Locale('es', 'MX'),
+      home: //const IniciarSesion(title: "Iniciar sesion"),
+      const Navegador()
+    );
+  }
+}
